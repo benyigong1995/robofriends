e613f6d11a40186ece1c83a6140c5d783705c4c5
Benyi Gong <benyigong1995@gmail.com>
2020-05-20 13:28:43 -0700

    Updates

---
+++ precache-manifest.27062ecb19fd190c3b5b8a0697d9ca8e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c9351f876f3ed99e81f26169205f814",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "cefaa6c9c2ed05ce5e33",
+    "url": "/static/css/main.ee72b140.chunk.css"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/static/js/2.88b08eb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.88b08eb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cefaa6c9c2ed05ce5e33",
+    "url": "/static/js/main.caf5f7d0.chunk.js"
+  },
+  {
+    "revision": "fdb66bf0534857f19626",
+    "url": "/static/js/runtime-main.8f3ae637.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
